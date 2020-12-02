8abb0f0f6bafbadf51eeafc4da4e4ce5b45b613c
oliviasherwood333 <72411648+oliviasherwood333@users.noreply.github.com>
2020-12-02 21:06:21 +0000

    favicons and footer

--- script.js
+++ script.js
@@ -12,10 +12,8 @@ menuButton.addEventListener('click', () => {
 
 
 
-
-
 // grab a reference to all the elements that will control the show/hide of the nav
-const navIcons = document.querySelectorAll('menu-btn .js-nav-icon');
+const navIcons = document.querySelectorAll('.menu-btn.js-nav-icon')
 
 // loop through each of the nav icon triggers and add an event listener
 navIcons.forEach(function (icon) {
@@ -24,13 +22,6 @@ navIcons.forEach(function (icon) {
 
 // create the event handler function to toggle the active state of the site nav
 function toggleNav(event) {
-	const siteNav = document.querySelector('user-nav-open');
+	const siteNav = document.querySelector('.user-nav-open')
 	siteNav.classList.toggle('is-active');
 }
-
-
-
-// Form spree
-window.formbutton=window.formbutton||function(){(formbutton.q=formbutton.q||[]).push(arguments)};
-formbutton("create", {action: "https://formspree.io/f/mgepllab"})
-
